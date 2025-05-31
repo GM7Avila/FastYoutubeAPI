330a88921c64ee7588d51805bc2d5fe0bc9e3d6d
GM7Avila <guilherme.medeiros.avila@gmail.com>
2025-05-31 19:44:45 -0300

    chore: config.py exception key value

--- backend/application/core/config.py
+++ backend/application/core/config.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings
-from .paths import AppPaths
+from pydantic import ValidationError
 
+from .paths import AppPaths
 class Settings(BaseSettings):
     youtube_api_key: str
     # database_url: str
@@ -8,8 +9,14 @@ class Settings(BaseSettings):
     class Config:
       env_file = str(AppPaths().core_dir / ".env") # Path to .env (string)
 
-# Singleton Instance
-settings = Settings()
+try: 
+  settings = Settings()   # Singleton Instance
+except ValidationError as e:
+   raise RuntimeError(
+    "A variável de ambiente `YOUTUBE_API_KEY` está ausente ou inválida.\n\n"
+    "Verifique se o arquivo em `application/core/.env` está criado e devidamente"
+    "configurado.\n")
+
 
 # Test
 if __name__ == "__main__":
